1d9218900387b0c838739af623d725d936017aa9
James Fleming <james@electronic-quill.net>
2019-10-17 22:22:31 +0200

    Update shell.nix and add requirements.txt for python

--- migration/shell.nix
+++ migration/shell.nix
@@ -1,19 +1,21 @@
 with import <nixpkgs> {};
 
 stdenv.mkDerivation rec {
-    name = "syscat";
+    name = "syscat_migration";
 
     buildInputs = [
-        pkgs.python36Packages.requests
-        pkgs.python36Packages.pylint
-        pkgs.python3
-        pkgs.bash
+        pkgs.python37Full
+        pkgs.python37Packages.pip
+        pkgs.python37Packages.virtualenv
     ];
-
-    shellHook = "export PS1='\n\\[\\033[01;32m\\][nix webcat] \\w\\$\\[\\033[00m\\] '";
 
     env = buildEnv {
         name = name;
         paths = buildInputs;
     };
+
+    shellHook = "export PS1='\n\\[\\033[01;32m\\][nix syscat migration] \\w\\$\\[\\033[00m\\] ';\
+                 export PYTHONPATH=$PWD/venv/lib/python3.7/site-packages/:$PYTHONPATH;
+                 unset SOURCE_DATE_EPOCH";
+
 }
